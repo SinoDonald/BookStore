ceeed160dec577fdb90f0be60edfd82190926531
DONALD-GF75-9SC\Donald <lugenius2001@gmail.com>
2022-03-03 23:44:40 +0800

    改寫Create、Edit、Delete, 透過AngularJS成功

--- BookStore/Scripts/WebApp/Book1/EditCtrl.js
+++ BookStore/Scripts/WebApp/Book1/EditCtrl.js
@@ -1,27 +1,34 @@
 ﻿var app = angular.module('app', []);
+
 app.run(['$http', '$window', function ($http, $window) {
     $http.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $http.defaults.headers.common['__RequestVerificationToken'] = $('input[name=__RequestVerificationToken]').val();
 }]);
+
 app.service('appService', ['$http', function ($http) {
+
     this.GetBook = function (o) {
         return $http.post("Book1/GetBook", o);
     };
 
-    this.EditBook = function (o) {
-        return $http.post('Book1/UpdateBook', o)
-    }
+    this.UpdateBook = function (o) {
+        return $http.post('Book1/UpdateBook', o);
+    };
 }]);
-app.controller('EditCtrl', ['$scope', '$window', 'appService', '$rootScope' function ($scope, $window, appService, $rootScope) {
+
+app.controller('EditCtrl', ['$scope', '$window', 'appService', '$rootScope', function ($scope, $window, appService, $rootScope) {
+
     $scope.Book = {};
 
-    $scope.EditBook = function () {
-        appService.UpdateBook($scope.Book).then(function (ret) {
-            $window.location.href = '/Book1/Index';
-        })
-    }
     appService.GetBook({ id: $window.bookid })
         .then(function (ret) {
             $scope.Book = ret.data;
         });
+
+    $scope.CallUpdateBook = function () {
+        appService.UpdateBook($scope.Book).then(function (ret) {
+            $window.location.href = '/Book1/Index';
+        });
+        //$window.location.href = '/Book1/Index';   
+    }
 }]);
